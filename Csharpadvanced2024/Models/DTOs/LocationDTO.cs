87f7852c369297e1134ddf274b492eee7f5f9865
Matthew <104033082+MatthewH03@users.noreply.github.com>
2024-04-14 00:00:43 +0200

    subtitle vergeten toe te voegen in location dto, deze is hier nu toegevoegd

--- Csharpadvanced2024/Models/DTOs/LocationDTO.cs
+++ Csharpadvanced2024/Models/DTOs/LocationDTO.cs
@@ -5,6 +5,7 @@ namespace Csharpadvanced2024.Models.DTOs
     public class LocationDTO
     {
         public string Title { get; set; }
+        public string SubTitle { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         public string LandLordUrl { get; set; }
